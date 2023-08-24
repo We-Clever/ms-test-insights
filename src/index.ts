94be8c55a1dabe6d930ada27e8f7f1dd0c6abf90
Vinícius Assis <vinicius.assis@weclever.co>
2023-08-24 16:40:27 -0300

    refactor: fix index lint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express"
-import router from "./routes/routes";
-import cors from "cors";
-import * as dotenv from "dotenv";
-import ServerlessHttp from "serverless-http";
-import cookieParser from "cookie-parser";
+import express from 'express'
+import router from './routes/routes'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+import ServerlessHttp from 'serverless-http'
+import cookieParser from 'cookie-parser'
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
 app.use(cors())
 app.use(express.json())
@@ -14,6 +14,6 @@ app.use(express.raw({ limit: '50mb' }))
 app.use(express.text())
 app.use(express.urlencoded({ limit: '50mb', parameterLimit: 5000, extended: true }))
 app.use(cookieParser())
-app.use(router);
+app.use(router)
 
-module.exports.handler = ServerlessHttp(app);
+module.exports.handler = ServerlessHttp(app)
---
+++ src/routes/routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express"
+import jwt from 'jsonwebtoken'
+
+const router = Router()
+
+const authorizationJwtAndHost = (req: any, res: any, next: any) => {
+    const authHeader = req.headers.authorization;
+    let referer = req.headers.referer || req.headers.referrer || req.headers.Referer
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, String(process.env.SECRETJWT), (err: any, user: any) => {
+            if (err)
+                return res.status(403).send('error on validation token...');
+            req.user = user;
+            next();
+        });
+    } else if (referer && (referer.includes("weclever.co") || referer.includes("cleversale.com.br") || referer.includes("clever.live"))) {
+        next();
+    } else {
+        res.status(401).send('invalid token...')
+    }
+};
+
+export default router
